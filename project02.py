03e048740c45bc2de10273036858223875f96558
sanch367 <115811090+sanch367@users.noreply.github.com>
2022-10-14 12:17:19 -0400

    Update project02.py

--- project02.py
+++ project02.py
@@ -4,9 +4,10 @@ ENGR 130 Program Description
 	replace this text with your program description as a comment
 
 Assignment Information
-	Assignment:     Project 2
-	Author:         Name, login@purdue.edu
-	Team ID:        ## 
+	Assignment:     Project 2 Team 19
+	Author:         Braden Sanchez, David Hudson, Paulo Ramirez, Joshua Hirchak,
+			sanch367@purdue.edu, hudso166@purdue.edu, ramir378@purdue.edu, jhircha@purdue.edu
+	Team ID:        19
 	
 
 =+=============================================================================
