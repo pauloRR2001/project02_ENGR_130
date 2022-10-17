cedea092e73316c1cd049e5b8a21eabe5995c0d6
pauloRR2001 <115810645+pauloRR2001@users.noreply.github.com>
2022-10-17 12:16:24 -0400

    Update pathFinder.py

--- pathFinder.py
+++ pathFinder.py
@@ -1,26 +1,13 @@
-'''
-===============================================================================
-ENGR 130 Program Description 
-	replace this text with your program description as a comment
-
-Assignment Information
-	Assignment:     e.g.  Exam 1 - Question 16
-	Author:         Name, login@purdue.edu
-	Team ID:        ## 
-	
-
-=+=============================================================================
-'''
 import numpy as np 
 
 map = np.array([[0,0,0,0,0],
                 [0,1,'s',1,0],
                 [0,1,1,1,0],
                 [0,0,0,0,0],
-                [0,0,0,0,0],
-                [1,1,1,1,0],
-                [0,0,0,1,0],
-                [0,1,'f',1,0],
+                [1,1,0,0,0],
+                ['f',1,1,1,0],
+                [0,1,0,1,0],
+                [0,1,0,1,0],
                 [0,1,1,1,0],
                 [0,0,0,0,0]])
 
@@ -52,70 +39,55 @@ for row in map:
             end.append(i - 1)
             end.append(j - 1)
 
-print(goal)
-print(end)
-print(map[goal[0]][goal[1]])
-print(map[end[0]][end[1]])
-
 x = goal[0]
 y = goal[1]
 
+i_bound, j_bound = map.shape
+
+face = 'up'
 loop = 0
-while loop < 100:
+while loop < 800:
     loop += 1
     
+    print([x,y])
     if map[x][y] == 'f':
         print('goooooooooooooooooooallllllllll')
         break
-    
-    if map[x-1][y] == str(0):
-        print('up')
+        
+    if (face == 'up') and ((y < (j_bound - 1) and map[x][y+1] != str(1))):
         x -= 1
-        print([x,y])
-        continue
-        
-    if map[x][y+1] == str(0):
-        print('right')
+    if (face == 'right') and ((x < (i_bound - 1)) and map[x+1][y] != str(1)):
         y += 1
-        continue
-        
-    if map[x+1][y] == str(0):
-        print('down')
-        x += 1
-        continue
-        
-    if map[x][y-1] == str(0):
-        print('left')
+    if (face == 'down') and ((y > 0) and map[x][y-1] != str(1)):
+        x += 1 
+    if (face == 'left') and ((x > 0) and map[x-1][y] != str(1)):
         y -= 1
-        continue
     
-
-print(map[x][y])
-
-
-
-
-
-# write your code here (and delete this comment)
-# ----------------------------
-# Inputs
-# ----------------------------
-
-# ----------------------------
-# Computations
-# ----------------------------
-
-# ----------------------------
-# Outputs
-# -----------------------------
+    
+    
+    if ((y < (j_bound - 1) and map[x][y+1] != str(1))) and (face == 'up'):
+        print('face right')
+        face = 'right'
+        
+    if ((x < (i_bound - 1)) and map[x+1][y] != str(1)) and (face == 'right'):
+        print('face down')
+        face = 'down'
+        
+    if ((y > 0) and map[x][y-1] != str(1)) and (face == 'down'):
+        print('face left')
+        face = 'left'
+    
+    if ((x > 0) and map[x-1][y] != str(1)) and (face == 'left'):
+        print('face up')
+        face = 'up'
 
 
-'''
-===============================================================================
-ACADEMIC INTEGRITY STATEMENT
-    I have not used source code obtained from any other unauthorized
-    source, either modified or unmodified. Neither have I provided
-    access to my code to another. The project I am submitting
-    is my own original work.
-===============================================================================
-'''
+print('goal:', goal, 'end:', end)  
+print('x and y:', [x,y])
+print('slot value:', map[x][y])
+print('bounds:', [i_bound,j_bound])
+
+
+
+
+
