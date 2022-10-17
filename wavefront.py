2af9e220aa921e2401438e414616f3d57d8aafe9
pauloRR2001 <115810645+pauloRR2001@users.noreply.github.com>
2022-10-16 21:55:45 -0400

    Add files via upload

---
+++ wavefront.py
@@ -0,0 +1,42 @@
+'''
+===============================================================================
+ENGR 130 Program Description 
+	replace this text with your program description as a comment
+
+Assignment Information
+	Assignment:     e.g.  Exam 1 - Question 16
+	Author:         Name, login@purdue.edu
+	Team ID:        ## 
+	
+
+=+=============================================================================
+'''
+did you complete the information above? delete this line if yes
+
+
+
+
+
+# write your code here (and delete this comment)
+# ----------------------------
+# Inputs
+# ----------------------------
+
+# ----------------------------
+# Computations
+# ----------------------------
+
+# ----------------------------
+# Outputs
+# -----------------------------
+
+
+'''
+===============================================================================
+ACADEMIC INTEGRITY STATEMENT
+    I have not used source code obtained from any other unauthorized
+    source, either modified or unmodified. Neither have I provided
+    access to my code to another. The project I am submitting
+    is my own original work.
+===============================================================================
+'''
