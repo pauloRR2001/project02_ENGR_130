504d3d2667c5ea54b6562bca47b35280dd649fad
DavidHud <115820376+DavidHud@users.noreply.github.com>
2022-10-17 11:56:29 -0400

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -20,6 +20,13 @@ Purpose: Checks if the input is an integer and outputs error if it is not
 Author: ramir378
 ------------------------------------
 """
+from microbit import *
+import radio
+from time import sleep_us
+from machine import time_pulse_us
+
+chnl = 19 #change channel to your team number
+
 def forward(unit):
 	#wheel circumference = 20 cm
 	#one block = 17.78 cm
@@ -63,6 +70,66 @@ def turnRight90(unit):
 		motor[motorB] = 0
 		motor[motorC] = 0
 		wait1Msec(500)
+
+import robotbit_library as r
+# Define Ports for the bank of high current output
+M1A = 0x1
+M1B = 0x2
+M2A = 0x3
+M2B = 0x4
+r.setup()
+
+radio.config(channel=chnl)
+radio.on()
+
+def Drive(lft,rgt):
+    r.motor(M2B, lft)
+    r.motor(M1A, rgt)
+
+TIME_OUT = 100000  # Increase time out to see farther, but
+# this will reduce the sample rate
+ECHO = pin1      # ping sensor uses a single pin for ECHO and Trigger
+TRIGGER = pin1
+def distance(tp, ep):
+    # tp is the trigger pin and ep is the echo pin
+    ep.read_digital()  # clear echo
+    tp.write_digital(1)  # Send a 10 microSec pulse
+    sleep_us(10)  # wait 10 microSec
+    tp.write_digital(0)  # Send pulse low
+    ep.read_digital()  # clear echo signal - This is needed for a
+    # pingSensor
+    ts = time_pulse_us(ep, 1, TIME_OUT)  # Wait for echo or time out
+    if ts > 0:
+        ts = ts * 17 // 100  # if system did not timeout, then send
+        # back a scaled value
+    return ts  # Return timeout error as a negative number (-1)
+while True:
+    s = radio.receive()
+    if s is not None:
+        dist = distance(TRIGGER, ECHO)  # set up to read as a ping)))
+        # sensor same pin
+        print(dist)    # use serial terminal to get information
+        sleep(500)     # set for a slow update rate
+        
+        if s=="N":
+            Drive(-255,255)
+            display.show(Image.ARROW_N)
+        elif s=="S":
+            Drive(255,-255)
+            display.show(Image.ARROW_S)
+        elif s=="E":
+            Drive(-255,-255)
+            display.show(Image.ARROW_E)
+        elif s=="W":
+            Drive(255,255)
+            display.show(Image.ARROW_W)
+        if dist < 100:
+            Drive(0,0)
+            display.show(Image.HEART)
+            obstacle = 0
+    else:
+        Drive(0,0)
+    sleep(20)
 
 '''
 ===============================================================================
