fdb77301c06386eeabb35b78bba3fcfb3c24928c
DavidHud <115820376+DavidHud@users.noreply.github.com>
2022-10-17 23:12:39 -0400

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -20,148 +20,58 @@ Purpose: Checks if the input is an integer and outputs error if it is not
 Author: ramir378
 ------------------------------------
 """
+
+
 from microbit import *
-import radio
-from time import sleep_us
-from machine import time_pulse_us
-
-def forward(unit):
-	#wheel circumference = 20 cm
-	#one block = 17.78 cm
-	countsToTravel = (17.78/20) * (360) * unit
-	
-	nMotorEncoder[motorB] = 0
-	nMotorEncoder[motorC] = 0
-	nMotorEncoderTarget[motorB] = countsToTravel
-	nMotorEncoderTarget[motorC] = countsToTravel
-	motor[motorB] = 50
-	motor[motorC] = 50
-	while(nMotorRunState[motorB] != runStateIdle && nMotorRunState[motorC] != runStateIdle):
-		motor[motorB] = 0
-		motor[motorC] = 0
-		wait1Msec(500)
-
-def turnLeft90(unit):
-	countsToTravel = (17.78/20) * (360) * unit
-	
-	nMotorEncoder[motorB] = 0
-	nMotorEncoder[motorC] = 0
-	nMotorEncoderTarget[motorB] = countsToTravel
-	nMotorEncoderTarget[motorC] = countsToTravel
-	motor[motorB] = 50
-	motor[motorC] = -50
-	while(nMotorRunState[motorB] != runStateIdle && nMotorRunState[motorC] != runStateIdle):
-		motor[motorB] = 0
-		motor[motorC] = 0
-		wait1Msec(500)
-		
-def turnRight90(unit):
-	countsToTravel = (17.78/20) * (360) * unit
-	
-	nMotorEncoder[motorB] = 0
-	nMotorEncoder[motorC] = 0
-	nMotorEncoderTarget[motorB] = countsToTravel
-	nMotorEncoderTarget[motorC] = countsToTravel
-	motor[motorB] = -50
-	motor[motorC] = 50
-	while(nMotorRunState[motorB] != runStateIdle && nMotorRunState[motorC] != runStateIdle):
-		motor[motorB] = 0
-		motor[motorC] = 0
-		wait1Msec(500)
-
 import robotbit_library as r
-# Define Ports for the bank of high current output
+
+
+count = 0
 M1A = 0x1
 M1B = 0x2
 M2A = 0x3
 M2B = 0x4
 r.setup()
 
-
 def Drive(lft,rgt):
     r.motor(M2B, lft)
     r.motor(M1A, rgt)
 
-TIME_OUT = 100000  # Increase time out to see farther, but
-# this will reduce the sample rate
-ECHO = pin1      # ping sensor uses a single pin for ECHO and Trigger
-TRIGGER = pin1
-def distance(tp, ep):
-    # tp is the trigger pin and ep is the echo pin
-    ep.read_digital()  # clear echo
-    tp.write_digital(1)  # Send a 10 microSec pulse
-    sleep_us(10)  # wait 10 microSec
-    tp.write_digital(0)  # Send pulse low
-    ep.read_digital()  # clear echo signal - This is needed for a
-    # pingSensor
-    ts = time_pulse_us(ep, 1, TIME_OUT)  # Wait for echo or time out
-    if ts > 0:
-        ts = ts * 17 // 100  # if system did not timeout, then send
-        # back a scaled value
-    return ts  # Return timeout error as a negative number (-1)
-while True:
-    s = pathfinder()
-    if s is not None:
-        dist = distance(TRIGGER, ECHO)  # set up to read as a ping)))
-        # sensor same pin
-        print(dist)    # use serial terminal to get information
-        sleep(500)     # set for a slow update rate
-        
-        if s=="N":
-            Drive(-255,255)
+
+nextdirection = pathfinder()
+def movement(nextdirection):
+    
+    while True:
+        if nextdirection == "N":
+            Drive(-50,50)
             display.show(Image.ARROW_N)
-        elif s=="S":
-            Drive(255,-255)
-            display.show(Image.ARROW_S)
-        elif s=="E":
-            Drive(-255,-255)
-            display.show(Image.ARROW_E)
-        elif s=="W":
-            Drive(255,255)
-            display.show(Image.ARROW_W)
-        if dist < 100:
+            sleep(765)
             Drive(0,0)
-            display.show(Image.HEART)
-            obstacle = 0
-    else:
+            sleep(1000)
+            continue
+        elif nextdirection == "E":
+            Drive(-50,-50)
+            display.show(Image.ARROW_E)
+            sleep(365)
+        elif nextdirection == "W":
+            Drive(50,50)
+            display.show(Image.ARROW_W)
+            sleep(365)
+        elif nextdirection == "N":
+            Drive(-50,50)
+            display.show(Image.ARROW_N)
+            sleep(365)
+        elif nextdirection == "S":
+            Drive(-50,-50)
+            display.show(Image.ARROW_S)
+            sleep(725)
         Drive(0,0)
-    sleep(20)
-#microbit stuff
-from microbit import *
-import robotbit_library as r
+        sleep(1000)
+        Drive(-50,50)
+        display.show(Image.CHESSBOARD)
+        sleep(1000)
 
-M1A = 0x1
-M1B = 0x2
-M2A = 0x3
-M2B = 0x4
-r.setup()
-
-
-rot = (17.78/20) * (360)
-def Drive(lft,rgt):
-    r.motor(M2B, lft)
-    r.motor(M1A, rgt)
-def die():
-    Drive(0,0)
-
-while True:
-    Drive(-50.025,50)
-    sleep(725)
-    die()
-    if nextdirection == "E":
-        Drive(-50,-50)
-        display.show(Image.ARROW_E)
-        sleep(725)
-    elif nextdirection == "W":
-        Drive(50,50)
-        display.show(Image.ARROW_W)
-        sleep(725)
-    elif nextdirection == "N":
-        Drive(-50.025,50)
-        display.show(Image.ARROW_N)
-        sleep(725)
-        
-    sleep(1000)
+movement(nextdirection)
     
 '''
 ===============================================================================
