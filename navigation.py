96954b5f1886ea98c5736dd03f0dbd997fbcb180
jhirchak <115820383+jhirchak@users.noreply.github.com>
2022-10-18 21:50:06 -0400

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -21,7 +21,6 @@ Author: ramir378
 ------------------------------------
 """
 
-
 from microbit import *
 import robotbit_library as r
 
@@ -36,57 +35,218 @@ r.setup()
 def Drive(lft,rgt):
     r.motor(M2B, lft)
     r.motor(M1A, rgt)
+    return 0
 
 
 
-updateMap = [[11,12,13,14,0],
-            [10,1,99,1,0],
-            [9,1,1,1,0],
-            [8,7,6,7,0],
-            [7,6,5,6,0],
-            [6,5,4,5,0],
-            [5,4,3,4,0],
-            [4,3,2,3,0],
-            [5,4,3,4,0],
-            [0,0,0,0,0]]
+updateMap = [[11,12,13,14],
+            [10,1,99,1],
+            [9,1,1,1],
+            [8,7,6,7],
+            [7,6,5,6],
+            [6,5,4,5],
+            [5,4,3,4],
+            [4,3,2,3],
+            [5,4,3,4]]
 
+
+def driveTheCar(nextdirection):
+    
+    carAngle = 90
+    
+    if nextdirection == "N":
+        if carAngle == 90:
+            Drive(50,-50)
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+        elif carAngle == 0:
+            Drive(-50,-50) #should make function called turn left (Drive(-50, -50))
+            display.show(Image.ARROW_W)
+            sleep(380)
+            Drive(50,-50)
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+            carAngle = 90
+        elif carAngle == 180:
+            Drive(50,50) #should make function called turn right (Drive(50, 50))
+            display.show(Image.ARROW_W)
+            sleep(380)
+            Drive(50,-50)
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+            carAngle = 90
+        elif carAngle == 270:
+            Drive(-50,-50) #Turn left for twice as long 
+            display.show(Image.ARROW_S)
+            sleep(720)
+            Drive(0,0)
+            sleep(500)
+            Drive(50,-50)
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            display.show(Image.CHESSBOARD)
+            sleep(1000)
+            carAngle = 90
+
+
+    if nextdirection == "S":
+        if carAngle == 270:
+            Drive(50,-50)
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+        elif carAngle == 180:
+            Drive(50,50) #should make function called turn left (Drive(50, 50))
+            display.show(Image.ARROW_W)
+            sleep(380)
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+            carAngle = 270
+        elif carAngle == 0:
+            Drive(-50,-50) #should make function called turn right (Drive(-50, -50))
+            display.show(Image.ARROW_W)
+            sleep(380)
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+            carAngle = 270
+        elif carAngle == 90:
+            Drive(-50,-50) #Turn right twice as long 
+            display.show(Image.ARROW_W)
+            sleep(720)
+            Drive(0,0)
+            sleep(500)
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            display.show(Image.CHESSBOARD)
+            sleep(1000)
+            carAngle = 270
+
+    if nextdirection == "E":
+        if carAngle == 0:
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+        elif carAngle == 270:
+            Drive(50,50) #should make function called turn left (Drive(50, 50))
+            display.show(Image.ARROW_W)
+            sleep(380)
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+            carAngle = 0
+        elif carAngle == 90:
+            Drive(-50,-50) #should make function called turn right (Drive(-50, -50))
+            display.show(Image.ARROW_W)
+            sleep(380)
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+            carAngle = 0
+        elif carAngle == 180:
+            Drive(-50,-50) #Turn right twice as long 
+            display.show(Image.ARROW_W)
+            sleep(720)
+            Drive(0,0)
+            sleep(500)
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            display.show(Image.CHESSBOARD)
+            sleep(1000)
+            carAngle = 0
+            
+    if nextdirection == "W":
+        if carAngle == 180:
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+        elif carAngle == 90:
+            Drive(50,50) #should make function called turn left (Drive(50, 50))
+            display.show(Image.ARROW_W)
+            sleep(380)
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+            carAngle = 180
+        elif carAngle == 270:
+            Drive(-50,-50) #should make function called turn right (Drive(-50, -50))
+            display.show(Image.ARROW_W)
+            sleep(380)
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            sleep(1000)
+            carAngle = 180
+        elif carAngle == 0:
+            Drive(-50,-50) #Turn right twice as long 
+            display.show(Image.ARROW_W)
+            sleep(720)
+            Drive(0,0)
+            sleep(500)
+            Drive(50,-50) #Forward
+            display.show(Image.ARROW_N)
+            sleep(765)
+            Drive(0,0)
+            display.show(Image.CHESSBOARD)
+            sleep(1000)
+            carAngle = 180
+    else:
+        Drive(0,0)
+        
+    Drive(0,0)
     
 def pathReader(updateMap):
-    nextdirection = 'test'
-    i = -1
-    j = 0
+    nextdirection = ""
     start_i = 0
     start_j = 0
-    for row in updateMap:
-        i = i + 1
-        j= 0
-        for column in row:
-               j = j + 1
-               if column == 99:
-                   start_i = i
-                   start_j = j - 1
+    for row in range(len(updateMap)):
+        for col in range(len(updateMap[row])):
+            if updateMap[row][col] == 99:
+                start_i = row
+                start_j = col
+                
     i = start_i
+    print(i)
     j = start_j
+    print(j)
     
     n = 0
     listtt = []
-    countn = 0
-    counts = 0
-    countw = 0
-    counte = 0
-    rotation1 = 0
-    rotation2 = 0
-    while n < 10:
+    
+    while n < 15:
 
         north, south, east, west = 1000, 1000, 1000, 1000
-        print(nextdirection)
-        listtt.append(nextdirection)
-
-        movement(nextdirection) #callllllllll
-
-        n += 1
-        print(i, j)
-        if i!= 0:
+        
+            
+        if i != 0:
             north = updateMap[i-1][j]
             if north == 1:
                 north += 1000
@@ -96,123 +256,82 @@ def pathReader(updateMap):
             if south == 1:
                 south += 1000
 
-        if j!=0:
+        if j != 0:
             west = updateMap[i][j-1]
             if west == 1:
                 west += 1000
                 
-        if j!=3:   
+        if j != 3:   
             east = updateMap[i][j+1]
             if east == 1:
                 east += 1000
 
-        print(north, south, west, east)
-        
+        if i == start_i and j == start_j: #To make the first move when start is 99
+            if i != 0:
+                if north < south and north < west and north < east:
+                    i -= 1
+                    nextdirection = "N"
+            if i != 8:
+                if south < north and south < west and south < east:
+                    i += 1
+                    nextdirection = "S"
+            if j != 3:
+                if east < south and east < west and east < north:
+                    j += 1
+                    nextdirection = "E"
+            if j != 0:
+                if west < south and west < north and west < east:
+                    j -= 1
+                    nextdirection = "W"
+            
+            print(north, south, west, east)
+            
         if i != 0:
-            if (north < south) and (north < west) and (north < east) and (north != 1):
+            if north == updateMap[i][j] - 1:
                 i -= 1
-                display.show(Image.HAPPY)
                 nextdirection = "N"
-                
         
         if i != 8:
-            if (south < north) and (south < west) and (south < east) and (south != 1):
+            if south == updateMap[i][j] - 1:
                 i += 1
                 nextdirection = "S"
             
         if (j != 0):
-            if (west < south) and (west < north) and (west < east) and (west != 1):
+            if west == updateMap[i][j] - 1:
                 j -= 1
                 nextdirection = "W"
                 
         if j!=3:
-            if (east < south) and (east < west) and (east < north) and (east != 1):
+            if east == updateMap[i][j] - 1:
                 j += 1
                 nextdirection = "E"
-                
-    #David's failed if statements    
-        face = nextdirection
-        if nextdirection == 'W':
-            countw += 1
-        elif nextdirection == 'E':
-            counte += 1
-        elif nextdirection == 'S':
-            counts += 1
 
-        if countw%2 != 0:
-            nextdirection 
-        elif counte%2 != 0:
+        print(nextdirection)
+        listtt.append(nextdirection)
+        n += 1
+        print(i, j)       
+        driveTheCar(nextdirection)      
+    
 
-        elif counts%2 != 0:
-                nextdirection
+        
         
     Drive(0,0)
     print(listtt)
 
 
-def movement(nextdirection, rotation1, rotation2):
-    if nextdirection == "N":
-        Drive(50+rotation1,-50+rotation2)
-        display.show(Image.ARROW_N)
-        sleep(765)
-        Drive(0,0)
-        sleep(1000)
-        
-    elif nextdirection == "E":
-        Drive(-50,-50)
-        display.show(Image.ARROW_E)
-        sleep(380)
-        Drive(0,0)
-        sleep(500)
-        Drive(50,-50)
-        display.show(Image.ARROW_N)
-        sleep(765)
-        Drive(0,0)
-        display.show(Image.CHESSBOARD)
-        sleep(1000)
-        
-    elif nextdirection == "W":
-        Drive(50,50)
-        display.show(Image.ARROW_W)
-        sleep(380)
-        Drive(0,0)
-        sleep(500)
-        Drive(50,-50)
-        display.show(Image.ARROW_N)
-        sleep(765)
-        Drive(0,0)
-        display.show(Image.CHESSBOARD)
-        sleep(1000)
-        
-    elif nextdirection == "S":
-        Drive(-50,-50)
-        display.show(Image.ARROW_S)
-        sleep(720)
-        Drive(0,0)
-        sleep(500)
-        Drive(50,-50)
-        display.show(Image.ARROW_N)
-        sleep(765)
-        Drive(0,0)
-        display.show(Image.CHESSBOARD)
-        sleep(1000)
-        
-    elif nextdirection == "STOP":
-        Drive(0,0)
-        display.show(Image.HAPPY)
 
-    else:
-        Drive(0,0)
-        
-    Drive(0,0)
         
 
 def main():
-
-    pathReader(updateMap)
+    #pathReader(updateMap)
+    Drive(50, 50)
+    sleep(380)
+    Drive(0,0)
+    
 if __name__ == "__main__":
     main()
 
+           
 '''
 ===============================================================================
 ACADEMIC INTEGRITY STATEMENT
